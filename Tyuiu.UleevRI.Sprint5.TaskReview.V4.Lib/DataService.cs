ba78161e468669b4f9f21aa4ab916a43f7f1f840
Tyuiu.UleevRI@outlook.com <romanuleev178@gmail.com>
2023-12-02 09:50:21 +0500

    Add project files.

---
+++ Tyuiu.UleevRI.Sprint5.TaskReview.V4.Lib/DataService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.UleevRI.Sprint5.TaskReview.V4.Lib
+{
+    public class DataService : ISprint5Task7V4
+    {
+        public string LoadDataAndSave(string path)
+        {
+            string pathSaveFile = $@"{Directory.GetCurrentDirectory()}\OutPutDataFileTask7V4.txt";
+            FileInfo fileInfo = new FileInfo(pathSaveFile);
+            bool fileExists = fileInfo.Exists;
+            if (fileExists) File.Delete(pathSaveFile);
+            string s = "";
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        char c = line[i];
+                        if (((c >= 'А') && (c <= 'Я')) || ((c >= 'а') && (c <= 'я'))) s += "#";
+                        else s += c;
+                    }
+                    File.AppendAllText(pathSaveFile, s + Environment.NewLine);
+                    s = "";
+                }
+            }
+            return pathSaveFile;
+        }
+    }
+}
---
+++ Tyuiu.UleevRI.Sprint5.TaskReview.V4.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using Tyuiu.UleevRI.Sprint5.TaskReview.V4;
+
+namespace Tyuiu.UleevRI.Sprint5.TaskReview.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\roman\source\repos\Tyuiu.UleevRI.Sprint5.Review\Tyuiu.UleevRI.Sprint5.TaskReview.V4\bin\Debug\OutPutDataFileTask7V4.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
